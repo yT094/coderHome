b7fa80f7990e59b97927f3a7ba59e187a659a72c
coderycs <1748780248@qq.com>
2022-11-16 08:28:41 +0800

    feat: 注册用户-将数据写入数据库

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,6 +4,7 @@ class UserController {
   async create(ctx, next) {
     // 获取用户请求传递的参数
     const user = ctx.request.body;
+    console.log("user", user);
 
     // 查询数据
     const result = await service.create(user);
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,6 +1,14 @@
+const connections = require("../app/database");
+
 class UserService {
   async create(user) {
     console.log("将用户数据保存到数据库中: ", user);
+    const { name, password } = user;
+
+    const statement = `INSERT INTO users (name, password) VALUES (?,?);`;
+
+    const result = await connections.execute(statement, [name, password]);
+
     return "创建用户成功";
   }
 }
