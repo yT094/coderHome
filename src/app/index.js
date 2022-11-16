bc0aba92e7314bc65e95897377438cb821d82c03
coderycs <1748780248@qq.com>
2022-11-16 20:41:30 +0800

    perf: 优化 router 的使用

--- src/app/index.js
+++ src/app/index.js
@@ -2,18 +2,15 @@ const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const errorHandler = require("./error-handle");
 
-const userRouter = require("../router/user.router");
-const authRouter = require("../router/auth.router");
-
 const app = new Koa();
 
+const useRoutes = require("../router");
+
+// 保持队形 - useRoutes 用 this 绑定
+app.userRoutes = useRoutes;
+
 app.use(bodyParser());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-
-app.use(authRouter.routes());
-app.use(authRouter.allowedMethods());
-
+app.userRoutes();
 app.on("error", errorHandler);
 
 module.exports = app;
