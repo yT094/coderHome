4cdb8f2478e46be18a38da38644ae1571b3601cd
coderycs <1748780248@qq.com>
2022-11-27 16:43:11 +0800

    feat: 动态详情接口-增加 avatar_url 字段

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -19,13 +19,13 @@ class MomentService {
     const statement = `
     SELECT 
       m.id id, m.content content, m.createAt createTime, m.updateAt updateTime,
-      JSON_OBJECT('id', u.id, 'name', u.name) author,
+      JSON_OBJECT('id', u.id, 'name', u.name, 'avatar_url', u.avatar_url) author,
       IF(COUNT(l.id),JSON_ARRAYAGG(
         JSON_OBJECT('id', l.id, 'name', l.name)
       ),NULL) labels,
       (SELECT IF(COUNT(c.id),JSON_ARRAYAGG(
         JSON_OBJECT('id', c.id, 'content', c.content, 'commentId', c.comment_id, 'createTime', c.createAt,
-                    'user', JSON_OBJECT('id', cu.id, 'name', cu.name))
+                    'user', JSON_OBJECT('id', cu.id, 'name', cu.name, 'avatar_url', cu.avatar_url))
       ),NULL) FROM comment c LEFT JOIN users cu ON c.user_id = cu.id WHERE m.id = c.moment_id) comments       
     FROM moment m
     LEFT JOIN users u ON m.user_id = u.id
